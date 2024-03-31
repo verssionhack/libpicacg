014b8329e28bef10f3590a75b0bf2002ebd80764
yinpeach <yinpeach0314@gmail.com>
2024-03-31 14:47:57 +0800

    modified:   src/api_impl.rs

--- src/api_impl.rs
+++ src/api_impl.rs
@@ -272,7 +272,7 @@ impl Api {
     }
 
 
-    #[cfg(debug)]
+    #[cfg(feature="debug")]
     pub async fn send<T: Debug + DeserializeOwned>(&self, builder: RequestBuilder) -> ApiResult<T> {
         let req =  builder.build().unwrap();
         //println!("Request {}", req.url().as_str());
@@ -292,7 +292,7 @@ impl Api {
     }
 
 
-    #[cfg(release)]
+    #[cfg(not(feature="debug"))]
     pub async fn send<T: Debug + DeserializeOwned>(&self, builder: RequestBuilder) -> ApiResult<T> {
         Ok(builder.send().await?.json::<Response<T>>().await
             .map(|response| {
